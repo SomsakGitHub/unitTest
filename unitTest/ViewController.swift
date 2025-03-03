1b7e711dfe42b8992b9fb82df24d752a736e7994
somsak <somsakk@tisco.co.th>
2025-03-03 10:14:25 +0700

    init project.

---
+++ unitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unitTest
+//
+//  Created by somsak02061 on 3/3/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ unitTestTests/unitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  unitTestTests.swift
+//  unitTestTests
+//
+//  Created by somsak02061 on 3/3/2568 BE.
+//
+
+import Testing
+@testable import unitTest
+
+struct unitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
